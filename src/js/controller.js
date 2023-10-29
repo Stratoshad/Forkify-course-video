a913f7b76fdd58a4ee0b50c455dcefdce10e8c87
Stratoshad <stratosh@hotmail.com>
2023-10-29 15:58:32 +0200

    Cleanned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -137,6 +137,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Hello Console');
 };
 
 init();
