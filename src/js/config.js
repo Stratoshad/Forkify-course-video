f6d92af34165e3faf5efe2766663fb802b3beecb
Stratoshad <stratosh@hotmail.com>
2023-10-29 15:38:08 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 5;
+export const KEY = '9b6c2925-50b5-4833-9405-d8103ace791d';
+export const MODAL_CLOSE_SEC = 2.5;
